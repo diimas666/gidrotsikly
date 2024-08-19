fc47deaa30f062a2a123a4f376d4be83a29fa227
dima tikhtey <uu36548@gmail.com>
2024-08-19 18:01:19 +0300

    ползунок верстаем фильтр часть 2

--- js/main.js
+++ js/main.js
@@ -34,4 +34,10 @@ $(function(){
         $(this).next().slideToggle('200');
     });
 
+    $('.js-range-slider').ionRangeSlider({
+        grid: false, 
+        type:"double",
+        min:100000,
+        max:5000000,
+    });
 });
