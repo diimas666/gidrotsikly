9d1d4fa1c7ed3de78ddafce8aa88794038afcd8a
dima tikhtey <uu36548@gmail.com>
2024-09-11 10:52:25 +0300

    footer adapt finish

--- js/main.js
+++ js/main.js
@@ -105,4 +105,10 @@ $(function () {
   $('.menu__btn').on('click', function () {
     $('.menu-mobile__list').toggleClass('menu-mobile__list--active');
   });
+
+  $('.footer__topdrop').on('click', function(){
+    $(this).next().slideToggle();
+    $(this).toggleClass('footer__top-itemdrop--active');
+  });
+
 });
