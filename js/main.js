ce7ae9f7bd851d2dc1f6da5ab9bc83579e34983b
dima tikhtey <uu36548@gmail.com>
2024-09-13 10:40:23 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ $(function () {
 
     $(this).addClass('tab--active');
     $($(this).attr('href')).addClass('tabs-content--active');
+    $('.product-slider').slick('setPosition');
   });
 
   $('.product-item__favorite').on('click', function () {
@@ -38,36 +39,36 @@ $(function () {
       '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
     nextArrow:
       '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
-      responsive: [
-        {
-          breakpoint: 1301,
-          settings: {
-            arrows: false,
-            dots: true
-          },
+    responsive: [
+      {
+        breakpoint: 1301,
+        settings: {
+          arrows: false,
+          dots: true,
         },
-        {
-          breakpoint: 1201,
-          settings: {
-            slidesToShow: 3,
-            dots: true
-          },
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 3,
+          dots: true,
         },
-        {
-          breakpoint: 870,
-          settings: {
-            slidesToShow: 2,
-            dots: true
-          },
+      },
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
         },
-        {
-          breakpoint: 590,
-          settings: {
-            slidesToShow: 1,
-            dots: true
-          },
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
         },
-      ],
+      },
+    ],
   });
 
   $('.filter-style').styler();
@@ -106,14 +107,12 @@ $(function () {
     $('.menu-mobile__list').toggleClass('menu-mobile__list--active');
   });
 
-  $('.footer__topdrop').on('click', function(){
+  $('.footer__topdrop').on('click', function () {
     $(this).next().slideToggle();
     $(this).toggleClass('footer__top-itemdrop--active');
   });
 
-
-  $('.aside__btn').on('click', function(){
+  $('.aside__btn').on('click', function () {
     $(this).next().slideToggle();
   });
-
 });
